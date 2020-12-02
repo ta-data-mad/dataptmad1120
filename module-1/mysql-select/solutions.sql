2b7ed1c675ad19d175d9439230c8b166920de74c
marinafdezban <m.fernandezban@gmail.com>
2020-12-02 19:50:31 +0100

    lab-wip

---
+++ module-1/mysql-select/solutions.sql
@@ -0,0 +1,10 @@
+EJERCICIO 1
+
+SELECT * FROM titleauthor;
+
+
+SELECT au_id, au_lname as 'Last Name', au_fname as 'First Name', title, pub_name as 'Publishers'
+FROM authors, titles, publishers;
+
+
+EJERCICIO 2
