58b4ae6bfb4673f8826881f9b856d73c9d048427
marinafdezban <m.fernandezban@gmail.com>
2020-12-04 22:17:55 +0100

    lab-wip

--- module-1/mysql-select/solutions.sql
+++ module-1/mysql-select/solutions.sql
@@ -1,10 +1,17 @@
-EJERCICIO 1
-
 SELECT * FROM titleauthor;
 
-
-SELECT au_id, au_lname as 'Last Name', au_fname as 'First Name', title, pub_name as 'Publishers'
-FROM authors, titles, publishers;
+SELECT authors.au_id as 'Author ID', 
+au_lname as 'Last Name', 
+au_fname as 'First Name', 
+title as Title,
+publishers.pub_name as Publishers
+FROM titles
+JOIN titleauthor
+ON titles.title_id = titleauthor.title_id 
+JOIN authors
+ON authors.au_id = titleauthor.au_id 
+JOIN publishers
+ON titles.pub_id = publishers.pub_id;
 
 
 EJERCICIO 2
