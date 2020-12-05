50aaf01c8cbe19b13eee07698a276e5449cb27fc
marinafdezban <m.fernandezban@gmail.com>
2020-12-05 11:03:18 +0100

    lab-wip

--- module-1/mysql-select/solutions.sql
+++ module-1/mysql-select/solutions.sql
@@ -15,3 +15,18 @@ ON titles.pub_id = publishers.pub_id;
 
 
 EJERCICIO 2
+
+SELECT authors.au_id as AUTHOR_ID, 
+authors.au_lname as LAST_NAME, 
+authors.au_fname as FIRST_NAME, 
+publishers.pub_name as PUBLISHER,
+COUNT(titles.title) as TOTAL_COUNT 
+FROM titles
+LEFT JOIN titleauthor 
+ON titles.title_id = titleauthor.title_id 
+LEFT JOIN authors
+ON titleauthor.au_id = authors.au_id 
+LEFT JOIN publishers
+ON publishers.pub_id = titles.pub_id   
+GROUP BY authors.au_id 
+ORDER BY TOTAL_COUNT DESC;
