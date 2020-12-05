2a043215923522b6e43972cf1997547292712e55
Miguel <jmiguelgonzalez1989@gmail.com>
2020-12-05 17:42:41 +0100

    lab-started

--- module-1/mysql-select/your-code/solutions.sql
+++ module-1/mysql-select/your-code/solutions.sql
@@ -6,3 +6,8 @@ FROM authors
 INNER JOIN  titleauthor ON authors.au_id = titleauthor.au_id 
 INNER JOIN  titles ON titleauthor.title_id = titles.title_id 
 INNER JOIN  publishers ON titles.pub_id = publishers.pub_id
+
+
+'Solution for exercise 2'
+
+
