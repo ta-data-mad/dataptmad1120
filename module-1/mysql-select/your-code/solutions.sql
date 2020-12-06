7a8653def8af44eb97f1519425666a31f86a400a
Miguel <jmiguelgonzalez1989@gmail.com>
2020-12-06 12:52:27 +0100

    some advances

--- module-1/mysql-select/your-code/solutions.sql
+++ module-1/mysql-select/your-code/solutions.sql
@@ -10,4 +10,23 @@ INNER JOIN  publishers ON titles.pub_id = publishers.pub_id
 
 'Solution for exercise 2'
 
+SELECT authors.au_id as "AUTHOR ID" , au_lname AS "LAST NAME" , au_fname AS "FIRST NAME", pub_name AS "PUBLISHER" , COUNT(title) as "TITLE COUNT" 
+FROM authors
+INNER JOIN  titleauthor ON authors.au_id = titleauthor.au_id 
+INNER JOIN  titles ON titleauthor.title_id = titles.title_id 
+INNER JOIN  publishers ON titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id , pub_name
+ORDER BY authors.au_id DESC
 
+
+'Solution for exercise 3'
+
+SELECT authors.au_id as "AUTHOR ID" , au_lname AS "LAST NAME" , au_fname AS "FIRST NAME", ytd_sales AS "TOTAL"
+FROM authors
+INNER JOIN  titleauthor ON authors.au_id = titleauthor.au_id 
+INNER JOIN  titles ON titleauthor.title_id = titles.title_id 
+GROUP BY title 
+ORDER BY TOTAL DESC
+LIMIT 3;
+
+'Solution for exercise 4'
