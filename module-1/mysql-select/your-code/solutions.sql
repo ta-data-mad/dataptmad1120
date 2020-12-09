7f24e078ad925db193e6632030744b066d9e8e6f
Miguel <jmiguelgonzalez1989@gmail.com>
2020-12-09 19:31:31 +0100

    lab-finished

--- module-1/mysql-select/your-code/solutions.sql
+++ module-1/mysql-select/your-code/solutions.sql
@@ -1,11 +1,12 @@
 
 'Solution for exercise 1'
 
-SELECT authors.au_id as "AUTHOR ID" , au_lname AS "LAST NAME" , au_fname AS "FIRST NAME" , title AS "TITLE" , pub_name AS "PUBLISHERS"
+SELECT authors.au_id as "AUTHOR ID" , au_lname AS "LAST NAME" , au_fname AS "FIRST NAME" , title AS "TITLE" , pub_name AS "PUBLISHER"
 FROM authors
 INNER JOIN  titleauthor ON authors.au_id = titleauthor.au_id 
 INNER JOIN  titles ON titleauthor.title_id = titles.title_id 
 INNER JOIN  publishers ON titles.pub_id = publishers.pub_id
+ORDER BY authors.au_id , publisher DESC;
 
 
 'Solution for exercise 2'
@@ -16,15 +17,16 @@ INNER JOIN  titleauthor ON authors.au_id = titleauthor.au_id
 INNER JOIN  titles ON titleauthor.title_id = titles.title_id 
 INNER JOIN  publishers ON titles.pub_id = publishers.pub_id
 GROUP BY authors.au_id , pub_name
-ORDER BY authors.au_id DESC
+ORDER BY authors.au_id DESC;
 
 
 'Solution for exercise 3'
 
-SELECT authors.au_id as "AUTHOR ID" , au_lname AS "LAST NAME" , au_fname AS "FIRST NAME", sum(ytd_sales) AS "TOTAL"
+SELECT authors.au_id as "AUTHOR ID" , au_lname AS "LAST NAME" , au_fname AS "FIRST NAME", sum(qty) AS "TOTAL"
 FROM authors
-INNER JOIN  titleauthor ON authors.au_id = titleauthor.au_id 
-INNER JOIN  titles ON titleauthor.title_id = titles.title_id 
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id 
+INNER JOIN titles ON titleauthor.title_id = titles.title_id 
+INNER JOIN sales ON  sales.title_id = titles.title_id 
 GROUP BY authors.au_id 
 ORDER BY TOTAL DESC
 LIMIT 3;
@@ -32,10 +34,11 @@ LIMIT 3;
 
 'Solution for exercise 4'
 
-SELECT authors.au_id as "AUTHOR ID" , au_lname AS "LAST NAME" , au_fname AS "FIRST NAME",  IFNULL(sum(ytd_sales), 0) AS "TOTAL"
+SELECT authors.au_id as "AUTHOR ID" , au_lname AS "LAST NAME" , au_fname AS "FIRST NAME",  IFNULL(sum(qty), 0) AS "TOTAL"
 FROM authors
-LEFT JOIN  titleauthor ON authors.au_id = titleauthor.au_id 
-LEFT JOIN  titles ON titleauthor.title_id = titles.title_id 
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id 
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id 
+LEFT JOIN sales ON  sales.title_id = titles.title_id 
 GROUP BY authors.au_id 
 ORDER BY TOTAL DESC;
 
