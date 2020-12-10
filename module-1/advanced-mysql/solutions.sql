9b8e8c2d4e5d353f6fa0e5c89c495b2c6945f158
alvarogmf <alvaro.gmf@gmail.com>
2020-12-10 20:04:57 +0100

    save 1

--- module-1/advanced-mysql/solutions.sql
+++ module-1/advanced-mysql/solutions.sql
@@ -39,25 +39,23 @@ group by step_1.title_id, step_1.au_id;
 Step 3:
 
 With step_1 as (
-SELECT 
-titleauthor.title_id as title_id,
-titleauthor.au_id as au_id,
-titles.advance * titleauthor.royaltyper / 100 as advance,
-titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty
-from titleauthor
-join titles
-on titles.title_id = titleauthor.title_id
-join sales 
-on sales.title_id = titles.title_id
-),
+	SELECT 
+	titleauthor.title_id as title_id,
+	titleauthor.au_id as au_id,
+	titles.advance * titleauthor.royaltyper / 100 as advance,
+	titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as sales_royalty
+	from titleauthor
+	join titles
+	on titles.title_id = titleauthor.title_id
+	join sales 
+	on sales.title_id = titles.title_id),
 step_2 as (
-SELECT step_1.title_id,
-step_1.au_id as au_id,
-step_1.advance as total_advance,
-sum(step_1.sales_royalty) as total_sales_royalty
-from step_1
-group by step_1.title_id, step_1.au_id
-)
+	SELECT step_1.title_id,
+	step_1.au_id as au_id,
+	step_1.advance as total_advance,
+	sum(step_1.sales_royalty) as total_sales_royalty
+	from step_1
+	group by step_1.title_id, step_1.au_id)
 Select
 step_2.au_id,
 (sum(step_2.total_advance)+sum(step_2.total_sales_royalty)) as total_profits
