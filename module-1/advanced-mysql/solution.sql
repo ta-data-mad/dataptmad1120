5f8787e1f1b6a38e5dbdf0ca7e49f03682af413f
markaw <markanthony.westmacott@gmail.com>
2020-12-11 07:56:26 +0100

    lab-finished

--- module-1/advanced-mysql/solution.sql
+++ module-1/advanced-mysql/solution.sql
@@ -1,20 +1,43 @@
-# CHALLENGE 1 
+# CHALLENGE_1 
 
-SELECT au_id, total_royalties + advance AS total_income
+SELECT au_id, sales_royalty_total + advance as profit
 FROM (
-SELECT title_id, SUM(sales_royalty) AS total_royalties, advance, au_id, title_id 
+SELECT title_id, au_id, SUM(sales_royalty) as sales_royalty_total, advance
 FROM (
-SELECT authors.au_id,
-	   titles.title_id,
-	   titles.advance * titleauthor.royaltyper /100 as advance,
-	   titles.price * sales.qty / 100 * titleauthor.royaltyper /100 AS sales_royalty
-FROM sales
-JOIN titles ON sales.title_id = titles.title_id
-JOIN titleauthor ON titles.title_id = titleauthor.title_id 
-JOIN authors ON authors.au_id = titleauthor.au_id
+SELECT titles.title_id, 
+	   titleauthor.au_id, 
+	   titles.advance * titleauthor.royaltyper / 100 as advance,
+	   titles.price * sales.qty * titles.royalty /100 * titleauthor.royaltyper / 100 as sales_royalty
+FROM titles
+JOIN titleauthor ON titles.title_id = titleauthor.title_id
+JOIN sales ON titles.title_id = sales.title_id
 )
-GROUP BY au_id, title_id 
+GROUP BY au_id, title_id
 )
-ORDER BY total_income DESC
+ORDER BY profit  DESC
+LIMIT 3
 
-#CHALLENGE 2 
+#CHALLENGE_2 
+#STEP_1
+
+CREATE TEMPORARY TABLE table_1 as 
+	   SELECT titles.title_id, 
+	          titleauthor.au_id, 
+	          titles.advance * titleauthor.royaltyper / 100 as advance,
+	          titles.price * sales.qty * titles.royalty /100 * titleauthor.royaltyper / 100 as sales_royalty
+       FROM titles
+       JOIN titleauthor ON titles.title_id = titleauthor.title_id
+       JOIN sales ON titles.title_id = sales.title_id
+       
+#STEP_2    
+CREATE TEMPORARY TABLE table_2 as 
+SELECT title_id, au_id, SUM(sales_royalty) as sales_royalty_total, advance
+FROM table_1
+GROUP BY au_id, title_id
+
+#STEP_3
+       
+SELECT au_id, sales_royalty_total + advance as profit
+FROM table_2
+ORDER BY profit  DESC
+LIMIT 3
