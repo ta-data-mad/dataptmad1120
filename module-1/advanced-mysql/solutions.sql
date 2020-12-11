147e0d9650b31c7aaecbe9c7945ee20ff40f68d3
EloyGomez <emoreno@outlook.dk>
2020-12-11 22:28:04 +0100

    lab-finished

--- module-1/advanced-mysql/solutions.sql
+++ module-1/advanced-mysql/solutions.sql
@@ -83,3 +83,35 @@ LIMIT 3;
 
 -- Challenge 3
 -- Step 1 --
+
+CREATE TABLE most_profiting_authors AS 
+SELECT
+titleauthor.title_id AS 'Title ID',
+titleauthor.au_id AS 'Author ID',
+titles.advance * titleauthor.royaltyper / 100 AS 'Advance',
+titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS 'Royalty'
+FROM titles
+INNER JOIN sales ON sales.title_id = titles.title_id
+INNER JOIN titleauthor ON titleauthor.title_id = sales.title_id
+ORDER BY titleauthor.title_id;
+
+-- Step 2 --
+
+SELECT "Title ID" AS 'Title ID',
+       "Author ID" AS 'Author ID',
+       "Advance",
+       SUM("Royalty") AS 'Royalties'
+FROM most_profiting_authors
+GROUP BY "Title ID",
+         "Author ID"
+ORDER BY "Title ID"
+
+
+-- Step 3 --
+
+SELECT "Author ID" AS 'Author ID',
+ROUND(SUM("Advance" + "Royalties"), 2) AS 'Profits'
+FROM most_profiting_authors
+GROUP BY "Author ID"
+ORDER BY "Profits" DESC
+LIMIT 3;
