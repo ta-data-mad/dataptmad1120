2599dda238da0fdf377f9f136170bc61f6049a43
Sara Hernández Reyes <sara.shr5@gmail.com>
2020-12-12 18:29:19 +0100

    lab-finished

--- module-1/advanced-mysql/your-code/solutions.sql
+++ module-1/advanced-mysql/your-code/solutions.sql
@@ -59,23 +59,12 @@ LIMIT 3;
 CHALLENGE 2
 
 #First, I have created the first temporary table:
-CREATE TEMPORARY TABLE step_1 AS
-SELECT
-titleauthor.title_id AS 'Title ID',
-titleauthor.au_id AS 'Author ID',
-(titles.advance * titleauthor.royaltyper / 100) AS 'Advance',
-(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS 'Royalty'
-FROM titles 
-INNER JOIN sales ON sales.title_id = titles.title_id 
-INNER JOIN titleauthor ON titleauthor.title_id = sales.title_id 
-ORDER BY titleauthor.title_id;
-
-#Then, the second one:
-CREATE TEMPORARY TABLE step_2 AS
-SELECT
-step_1.title_id AS 'Title ID',
-step_1.au_id AS 'Author ID',
-SUM(step_1.Royalty) AS Royalties
+CREATE TEMPORARY TABLE temp_table AS
+SELECT 
+title_id AS 'Title ID',
+au_id AS 'Author ID',
+Advance,
+SUM(Royalty) AS Royalties
 FROM
 (SELECT titleauthor.title_id,
 titleauthor.au_id,
@@ -83,34 +72,29 @@ titles.advance * titleauthor.royaltyper / 100 AS Advance,
 titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS Royalty
 FROM titles
 INNER JOIN sales ON sales.title_id = titles.title_id
-INNER JOIN titleauthor ON titleauthor.title_id = sales.title_id) step_1
-GROUP BY step_1.title_id,
-         step_1.au_id
-ORDER BY step_1.title_id;
+INNER JOIN titleauthor ON titleauthor.title_id = sales.title_id)
+GROUP BY title_id,
+         au_id
+ORDER BY title_id
 
-
-#At the end, I have do the query using results of both temporary tables:
-SELECT 
-step_2.'Author ID',
-ROUND(SUM(step_1.Advance + step_2.Royalties),2) AS Profits
-FROM step_1
-JOIN step_2 ON step_1.'Title ID' = step_2.'Title ID'
-GROUP BY step_2.'Author ID'
-ORDER BY Profits DESC
+#Then, I have do the query using results of both temporary tables:
+SELECT
+temp_table.'Author ID',
+ROUND(SUM(Advance + Royalties),2) AS Profits
+FROM temp_table
+GROUP BY temp_table.'Author ID'
+ORDER BY Profits DESC 
 LIMIT 3
-#I don't get the correct value and I don't know exactly why. 
-
 
 
 CHALLENGE 3
 CREATE TABLE most_profiting_authors AS 
-SELECT 
-step_2.'Author ID',
-ROUND(SUM(step_1.Advance + step_2.Royalties),2) AS Profits
-FROM step_1
-JOIN step_2 ON step_1.'Title ID' = step_2.'Title ID'
-GROUP BY step_2.'Author ID'
-ORDER BY Profits DESC
+SELECT
+temp_table.'Author ID',
+ROUND(SUM(Advance + Royalties),2) AS Profits
+FROM temp_table
+GROUP BY temp_table.'Author ID'
+ORDER BY Profits DESC 
 LIMIT 3
 
 
