fc3af9bbaaa4c019e43effb73c824abb5882f2c2
alejandramatiasmartin2 <73171652+alejandramatiasmartin2@users.noreply.github.com>
2020-12-14 19:46:11 +0100

    lab-started

--- module-1/advanced-mysql/your-code/solutions.sql
+++ module-1/advanced-mysql/your-code/solutions.sql
@@ -1,18 +1,63 @@
-#Challenge 1
+# CHALLENGE 1
 
-#Step1
+# Step 1
 
 SELECT
-titles.title_id  as "Title ID",
-titles.title as "Title",
-titleauthor.au_id as "Author ID",
-titles.advance * titleauthor.royaltyper / 100 as "Advance",
-titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as  "Royalty"
+titleauthor.title_id AS 'Title ID',
+titleauthor.au_id AS 'Author ID',
+titles.advance * titleauthor.royaltyper / 100 AS 'Advance',
+titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS 'Royalty'
 FROM titles
-JOIN titleauthor
-ON titles.title_id = titleauthor.title_id
-JOIN sales
-ON titleauthor.title_id = sales.title_id;
+INNER JOIN sales ON sales.title_id = titles.title_id
+INNER JOIN titleauthor ON titleauthor.title_id = sales.title_id
+ORDER BY titleauthor.title_id;
 
-#Step2
+# Step 2
+
+SELECT 
+step_1.title_id AS 'Title ID',
+step_1.au_id AS 'Author ID',
+--step_1.Advance,
+SUM(step_1.Royalty) AS Royalties
+FROM
+  (SELECT 
+   titleauthor.title_id,
+   titleauthor.au_id,
+   titles.advance * titleauthor.royaltyper / 100 AS Advance,
+   titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS Royalty
+   FROM titles
+   INNER JOIN sales ON sales.title_id = titles.title_id
+   INNER JOIN titleauthor ON titleauthor.title_id = sales.title_id) step_1
+GROUP BY 
+	step_1.title_id,
+         step_1.au_id
+ORDER BY step_1.title_id;
+
+# Step 3
+
+SELECT 
+step_2.au_id AS 'Author ID',
+ROUND(SUM(step_2.Advance + step_2.Royalties), 2) AS Profits
+FROM
+  (SELECT 
+   step_1.title_id,
+   step_1.au_id,
+   step_1.Advance,
+   SUM(step_1.Royalty) AS Royalties
+   FROM
+     (SELECT 
+      titleauthor.title_id,
+      titleauthor.au_id,
+      titles.advance * titleauthor.royaltyper / 100 AS Advance,
+      titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS Royalty
+      FROM titles
+      INNER JOIN sales ON sales.title_id = titles.title_id
+      INNER JOIN titleauthor ON titleauthor.title_id = sales.title_id) step_1
+   GROUP BY step_1.title_id,
+            step_1.au_id) step_2
+GROUP BY step_2.au_id
+ORDER BY Profits DESC
+LIMIT 3;
+
+
 
