c067f39ccee475347b7ad76cb656cdcddff14049
Alvaro Rodriguez <alvaro.rodriguezdelgado@gmail.com>
2021-01-04 19:43:15 +0100

    til 13

--- extra-work/m1/lab-errhand-listcomp/your-code/main.py
+++ extra-work/m1/lab-errhand-listcomp/your-code/main.py
@@ -8,44 +8,48 @@ print(my_listComprehension)
 
 #Insert here the module/library import statements 
 
-
+import random
+import os
 
 
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+square=[i**2 for i in range(20)]
+print(square)
 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+power_of_two=[2**i for i in range(50)]
+print(power_of_two)
 
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
-
-
+sqrt=[i**0.5 for i in range(100)]
+print(sqrt)
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+my_list=[-10+i for i in range(11)]
+print(my_list)
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
-
+odds=[i for i in range(1,101) if i%2!=0 ]
+print(odds)
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+divisible_by_seven=[i for i in range(1,1001) if i%7==0 ]
+print(divisible_by_seven)
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
@@ -54,43 +58,55 @@ print(my_listComprehension)
 
 teststring = 'Find all of the words in a string that are monosyllabic'
 
+vowels=['a','e','i','y','o','u']
 
-
+non_vowels=[i for i in teststring if i.lower() not in vowels]
+print(non_vowels)
+string_non_vowels=''
+for i in non_vowels:
+    string_non_vowels=string_non_vowels+i
+print(string_non_vowels)
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
+test_string='The Quick Brown Fox Jumped Over The Lazy Dog'
 
+capital_letters=[i for i in test_string if i!=i.lower()]
+print(capital_letters)
 
 
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
-
-
+sentence='The quick brown fox jumped over the lazy dog'
+non_consonants=['a','e','i','y','o','u',' ']
+consonants=[i for i in sentence if i.lower() not in non_consonants]
+print(consonants)
 
 
 #10. Find the folders you have in your madrid-oct-2018 local repo. Use files as name of the list.  
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
-
+files=[]
+print(files)
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
-
-
+random_lists=[[random.randint(0,100) for i in range(10)] for i in range(4)]
+print(random_lists)
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
-
-
+flatten_list= [k for i in list_of_lists for k in i]
+print(flatten_list)
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
@@ -189,3 +205,4 @@ Total_Marks = int(input("Enter Total Marks Scored: "))
 Num_of_Sections = int(input("Enter Num of Sections: "))
 
 
+'''HACER 10!!!!!!'''
