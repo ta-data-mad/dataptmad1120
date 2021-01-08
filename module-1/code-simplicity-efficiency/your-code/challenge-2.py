b7734df3b6da9d8dad8932dec9c9ba8899ffd242
rfminguez <rfminguez@gmail.com>
2021-01-08 20:58:23 +0100

    challenge-2 finished

--- module-1/code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-2.py
@@ -7,32 +7,36 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+import random
+import sys
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+
+def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
+	result = ""
+
+	for i in range(l):
+		result += random.choice(a)
+
+	return result
+
+
+def BatchStringGenerator(num_random_strings, min_string_len=8, max_string_len=12):
+	result = []
+	string_len_ranges = list(range(min_string_len, max_string_len + 1))
+
+	if len(string_len_ranges) == 0:
+		sys.exit('Incorrect min and max string lengths. Try again.')
+
+	for i in range(num_random_strings):
+		string_len = random.choice(string_len_ranges)
+		result.append(RandomStringGenerator(string_len))
+
+	return result
+
 
 a = input('Enter minimum string length: ')
 b = input('Enter maximum string length: ')
 n = input('How many random strings to generate? ')
 
 print(BatchStringGenerator(int(n), int(a), int(b)))
+
