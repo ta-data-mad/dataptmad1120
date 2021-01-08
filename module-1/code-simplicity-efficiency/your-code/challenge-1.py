7f62e52f989bd3afd8c4cd3cb4227df77258e7d5
rfminguez <rfminguez@gmail.com>
2021-01-08 19:50:14 +0100

    challenge-1 corrected

--- module-1/code-simplicity-efficiency/your-code/challenge-1.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-1.py
@@ -15,7 +15,7 @@ a = input('Please choose your first number (zero to five): ')
 b = input('What do you want to do? plus or minus: ')
 c = input('Please choose your second number (zero to five): ')
 
-numbers = {
+words_to_numbers = {
 'zero': 0,
 'one': 1,
 'two': 2,
@@ -28,15 +28,43 @@ numbers = {
 'nine': 9
 }
 
+numbers_to_words = {
+0: 'zero',
+1: 'one',
+2: 'two',
+3: 'three',
+4: 'four',
+5: 'five',
+6: 'six',
+7: 'seven',
+8: 'eight',
+9: 'nine',
+10: 'ten',
+11: 'eleven',
+12: 'twelve',
+13: 'thirteen',
+14: 'fourteen',
+15: 'fifteen',
+16: 'sixteen',
+17: 'seventeen',
+18: 'eighteen'
+}
+
 opps = ['plus', 'minus']
 
-if a not in numbers.keys() or c not in numbers.keys() or b not in opps:
+
+if a not in words_to_numbers.keys() or c not in words_to_numbers.keys() or b not in opps:
 	print("I am not able to answer this question. Check your input.")
 else:
+	# print("zero plus zero equals zero")
+
 	if b == 'plus':
-		print(numbers[a] + numbers[c])
+		print(f"{a} plus {c} equals {numbers_to_words[words_to_numbers[a] + words_to_numbers[c]]}")
 	elif b == 'minus':
-		print(numbers[a] - numbers[c])
+		if words_to_numbers[a] >= words_to_numbers[c]:
+			print(f"{a} minus {c} equals {numbers_to_words[words_to_numbers[a] - words_to_numbers[c]]}")
+		else:
+			print(f"{a} minus {c} equals negative {numbers_to_words[-1 * (words_to_numbers[a] - words_to_numbers[c])]}")
 
 print("Thanks for using this calculator, goodbye :)")
 
