7ccdf983e5a43590362d6f96afd0c4535ad9ea12
Miguel <jmiguelgonzalez1989@gmail.com>
2021-01-10 12:16:44 +0100

    lab-finished

--- module-1/code-simplicity-efficiency/your-code/challenge-3.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-3.py
@@ -12,19 +12,28 @@ The following function shows one way to solve the problem but the code is not id
 Refactor the code based on what you have learned about code simplicity and efficiency.
 """
 
-def my_function(X):
+# Some easy improvements to apply to this code are: provide a better name to the variables (e.g:"hyptenuses") and
+# give a different exit in case the input is lower than 5. 
+
+max_length = int(input("What is the maximal length of the triangle side? Enter an integer number larger than 5: "))
+
+def my_function(max_length):
     solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
+    for x in range(5, max_length):
+        for y in range(4, max_length):
+            for z in range(3, max_length):
                 if (x*x==y*y+z*z):
                     solutions.append([x, y, z])
-    m = 0
+                    
+    hypotenuses = []
     for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
-    return m
+        hypotenuse = max(solution)
+        hypotenuses.append(hypotenuse)
+        max_hypo = max(hypotenuses)
+    return max_hypo
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
+if max_length < 5:
+    print("Please enter an integer number larger than 5")
+else:
+    print("The longest side possible is ", my_function(max_length))
 
-print("The longest side possible is " + str(my_function(int(X))))
