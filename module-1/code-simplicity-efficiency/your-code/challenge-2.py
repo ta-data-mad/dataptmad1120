3352907cd02eda276a0f695f4f0de08e387f6395
JieDh <jie.dh13@gmail.com>
2021-01-12 20:18:17 +0100

    not sure about challenge 2

--- module-1/code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-2.py
@@ -6,6 +6,36 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+##SOLUTION
+#Import all modules at the beggining
+import random
+import sys
+import string #so we can change the list assigned to the variable a for a concatenated list with lowercase abc and numbers
+def RandomStringGenerator(l): #idk why l=12, I just delete and it works. Delete the variable a and it list, so we can directly assign to random.choice
+    s = ''
+    while len(s)<l:
+        s += random.choice(string.ascii_lowercase + string.digits)
+    return s
+
+def BatchStringGenerator(n, a=8, b=12):
+    r = []
+    for i in range(n):
+        if a < b:
+            c = random.choice(range(a, b))
+        elif a == b:
+            c = a
+        else:
+            sys.exit('Incorrect min and max string lengths. Try again.')
+        r.append(RandomStringGenerator(c))
+    return r
+
+a = input('Enter minimum string length: ')
+b = input('Enter maximum string length: ')
+n = input('How many random strings to generate? ')
+
+print(BatchStringGenerator(int(n), int(a), int(b)))
+
+##CODE TO SIMPLIFY
 
 def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
     p = 0
