5e37d3701c1ea9b399fac634b64b9260daebc122
Sara Hernández Reyes <sara.shr5@gmail.com>
2021-01-12 20:59:03 +0100

    lab-finished

--- module-1/code-simplicity-efficiency/your-code/challenge-3.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-3.py
@@ -1,31 +1,32 @@
-"""
-You are presented with an integer number larger than 5. Your goal is to identify the longest side possible in a right triangle whose sides are not longer than the number you are given.
-
-For example, if you are given the number 15, there are 3 possibilities to compose right triangles:
-
-1. [3, 4, 5]
-2. [6, 8, 10]
-3. [5, 12, 13]
-
-The following function shows one way to solve the problem but the code is not ideal or efficient.
-Refactor the code based on what you have learned about code simplicity and efficiency.
-"""
-
 def longest_side_triangle(max_len):
+    """"
+    This functions calculates the longest side possible in a right triangle whose 
+    sides are not longer than the number you are given. The number has to be up to 5.
     
-    posible_solutions = []
+    Example:
+    Input: 15
+    Output: 13
     
-    for c in range(5, max_len):
-        for a in range(4, max_len):
-            for b in range(3, max_len):
-                if (c**2 == a**2 + b**2):
-                    posible_solutions.append([a, b, c])
+    Posible_solutions = 
+    1. [3, 4, 5]
+    2. [6, 8, 10]
+    3. [5, 12, 13]
+    """
     
-    for number in posible_solutions:
+    solutions = []
+    
+    for hypotenuse in range(5, max_len):
+        for cathetus_a in range(5, max_len):
+            for cathetus_b in range(5, max_len):
+                if (hypotenuse**2 == cathetus_a**2 + cathetus_b**2):
+                    solutions.append([hypotenuse, cathetus_a, cathetus_b])
+                        
+    for number in solutions:
         longest_side = max(number)
                     
     return longest_side
 
 max_len = input("What is the maximal length of the triangle side? Enter a number: ")
-
 print("The longest side possible is " + str(longest_side_triangle(int(max_len))))
+
+#I have improving the readability of the code, but I'm sure the for loop can be more improved but I din't know how do it. 
