83b66396476ed0091496f4fb2af5c9ceb7e3abe4
alvarogmf <alvaro.gmf@gmail.com>
2021-01-12 21:38:42 +0100

    lab-finished

--- module-1/code-simplicity-efficiency/your-code/challenge-3.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-3.py
@@ -11,20 +11,19 @@ For example, if you are given the number 15, there are 3 possibilities to compos
 The following function shows one way to solve the problem but the code is not ideal or efficient.
 Refactor the code based on what you have learned about code simplicity and efficiency.
 """
+# first goes the input, the variable will be renamed to something easier to understand and add the int():
 
-def my_function(X):
+input_value = int(input("What is the maximal length of the triangle side? Enter a number: "))
+
+
+def my_function(input_value):
     solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
+    for x in range(5, input_value):
+        for y in range(4, input_value):
+            for z in range(3, input_value):
+                if (x**2 == y**2 + z**2):       # change the formula to be easier to understand
                     solutions.append([x, y, z])
-    m = 0
-    for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
-    return m
+    maximum = max([i for lst in solutions for i in lst])   # changed name m for maximum for better readability and changed to a list comprehension
+    return maximum
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
-
-print("The longest side possible is " + str(my_function(int(X))))
+print(f'The longest side possible is {my_function(input_value)}')  # converted to f-string
