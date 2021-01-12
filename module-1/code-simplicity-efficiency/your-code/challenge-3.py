af9fbb4e94ae6a19cb932fcc2a9348ddbb62dbec
CastillaAlvaro <castillasantos.a@gmail.com>
2021-01-12 12:51:12 +0100

    lab-finished

--- module-1/code-simplicity-efficiency/your-code/challenge-3.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-3.py
@@ -12,19 +12,23 @@ The following function shows one way to solve the problem but the code is not id
 Refactor the code based on what you have learned about code simplicity and efficiency.
 """
 
-def my_function(X):
+def RightTriangleLongestSide(max_length):
+    
     solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
+    
+    for x in range(5, max_lenght):
+        for y in range(4, max_lenght):
+            for z in range(3, max_lenght):
+                if (x*x == y*y + z*z):
                     solutions.append([x, y, z])
+    
     m = 0
+    
     for solution in solutions:
         if m < max(solution):
             m = max(solution)
     return m
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
+max_lenght = input("What is the maximal length of the triangle side? Enter a number: ")
 
-print("The longest side possible is " + str(my_function(int(X))))
+print("The longest side possible is " + str(RightTriangleLongestSide(int(max_lenght))))
