899e095ae5bed6b3e347b3bbf01aa16aac8e6458
VickyViana <mvictoria.viana@gmail.com>
2021-01-13 22:54:03 +0100

    challenge-2 finished

--- module-1/code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-2.py
@@ -6,33 +6,43 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+#The importations should be first
+import random
+import sys
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+#Functions redefined with lowercase and _.
+#Variables renamed more descriptively.
+#The list of elements that could be used to form the strings is defined apart as "letters" list.
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+   
+def random_string_generator(str_length , digits):
+    count = 0
+    created_str = ''
+    while count < str_length:
+        created_str += random.choice(digits)
+        count += 1
+    return created_str
+    
+
+def batch_string_generator(num_strings, shorter, longer):
+    final_list = []
+    for i in range(num_strings,):
+        actual_lenght = None
+        if shorter < longer:
+            actual_lenght = random.choice(range(shorter, longer))
+        elif shorter == longer:
+            actual_lenght = shorter
         else:
-            import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+        final_list.append(random_string_generator(actual_lenght, letters))
+    
+    return final_list
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+ 
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+min_length = input('Enter minimum string length: ')
+max_length = input('Enter maximum string length: ')
+list_length = input('How many random strings to generate? ')
+
+print(batch_string_generator(int(list_length), int(min_length), int(max_length)))
