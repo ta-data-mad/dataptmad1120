1ccdba33be9fd9507d4ac353eb93d187fb44099d
marinafdezban <m.fernandezban@gmail.com>
2021-01-14 14:01:32 +0100

    lab-wip

--- module-1/code-simplicity-efficiency/your-code/challenge-1.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-1.py
@@ -1,26 +1,20 @@
 print('Welcome to this calculator!')
 print('It can add and subtract whole numbers from zero to five')
-a = input('Please choose your first number (zero to five): ')
+
+
+a = int(input('Please choose your first number (zero to five): '))
 b = input('What do you want to do? plus or minus: ')
-c = input('Please choose your second number (zero to five): ')
+c = int(input('Please choose your second number (zero to five): '))
 
-# Addition
-print('{} plus {} = '.format(a, c))
-print(a + c)
 
-# Substraction
-print('{} minus {} = '.format(a, c))
-print(a - c)
-
-if operation == 'plus'
- print('{} plus {} = '.format(a, c))
- print(a + c)
-
-elif operation == 'minus'
- print('{} minus {} = '.format(a, c))
- print(a - c)
-
+if a >= 0 and a <= 5 and c >= 0 and c <= 5:
+    if b == "plus":
+        print (f'{a} {b} {c} equals {a + c}')
+    elif b == "minus":
+        print (f'{a} {b} {c} equals {a - c}')
+    else:
+        print("I am not able to answer this question. Check your input (minus or plus).")
 else:
- print("I am not able to answer this question. Check your input.")
+    print("I am not able to answer this question. Check your input (from zero to five).")
 
 print("Thanks for using this calculator, goodbye :)")
