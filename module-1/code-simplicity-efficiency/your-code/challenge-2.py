2e75e163b38f888c52934f50b208eb583e7bfe7f
Luis Muñoz <luismunnozromero@gmail.com>
2021-01-17 18:55:54 +0100

    lab-finished

--- module-1/code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-2.py
@@ -2,37 +2,47 @@
 The code below generates a given number of random strings that consists of numbers and 
 lower case English letters. You can also define the range of the variable lengths of
 the strings being generated.
-
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
+import random
+import sys
+#for proper libraries
+
+#clearer definitions and we locate our inputs and rename them
+str_len = 12
+str_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+min_len = input('Enter minimum string len: ')
+max_len = input('Enter maximum string len: ')
+str_number = int(input('How many random strings to generate? '))
+
+
+#variables renamed
+
+def RandomStringGenerator(str_len, str_list):
     p = 0
     s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
+    while p < str_len:
+        s += random.choice(str_list)
         p += 1
-    return s
-
-def BatchStringGenerator(n, a=8, b=12):
+    return s    
+    
+    
+def BatchStringGenerator(n, min_lenght=8, max_lenght=12):
     r = []
-    for i in range(n):
+    for i in range(str_number):
         c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+        if min_len < max_len:
+            c = random.choice(range(min_len, max_len))
+        elif min_len == max_len:
+            c = min_len
         else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
+            sys.exit('Incorrect min and max string lens. Try again.')
+        r.append(RandomStringGenerator(c,str_list))
     return r
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+
+print(BatchStringGenerator(int(str_number), int(min_len), int(max_len)))
+                
--- module-1/code-simplicity-efficiency/your-code/challenge-3.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-3.py
@@ -12,19 +12,19 @@ The following function shows one way to solve the problem but the code is not id
 Refactor the code based on what you have learned about code simplicity and efficiency.
 """
 
-def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                    solutions.append([x, y, z])
-    m = 0
-    for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
-    return m
+#imput variable renamed
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
+def my_function(input):
+    solution = []
+    for x in range(5, input):
+        for y in range(4, input):
+            for z in range(3, input):
+#formula in a clever way
+                if (x**2 == y**2 + z**2):     
+                    solution.append([x, y, z])
+#list comprehension used                   
+#variable m renamed
+    maximum = max([i for lst in solution for i in lst])  
+    return maximum
 
-print("The longest side possible is " + str(my_function(int(X))))
+print(f'The longest side possible is {my_function(value)}'
