b2ffc8f7c17ae6aeba148336cfdcf28ce71785ee
marinafdezban <m.fernandezban@gmail.com>
2021-03-02 21:49:48 +0100

    lab-updated

--- module-2/intro-bi-tableau/main.py
+++ module-2/intro-bi-tableau/main.py
@@ -1,4 +1,4 @@
 """I had some issues trying to upload the Tableau lab. 
 Finally I could do it by uploading chart by chart of the lab. 
-Link:https://public.tableau.com/profile/marina5504#!/
+Link:https://public.tableau.com/profile/marina5504#!/vizhome/labintro-bi-tableau/TotalRetailTransfersbyYearQuarter/
 """
